a9f8f9d0e3ad6e5f37ae3ee563f8c543a2e188e3
Evgeny <evgeny@MacBook-Air-Evgeny.local>
2024-05-20 17:13:50 +0700

    getting all tags array

--- src/tag/tag.module.ts
+++ src/tag/tag.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TagEntity } from './entities/tag.entity';
 import { TagController } from './tag.controller';
 import { TagService } from './tag.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TagEntity])],
   controllers: [TagController],
   providers: [TagService]
 })
--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TagEntity } from './entities/tag.entity';
 
 @Injectable()
 export class TagService {
-    findAll() {
-        return ["one", "two", "three"]
+
+    constructor(
+        @InjectRepository(TagEntity)
+        private readonly tagRepository: Repository<TagEntity>
+    ){}
+
+    async findAll(): Promise<{tags: string[]}> {
+        const tags = await this.tagRepository.find()
+        return {
+            tags: tags.map(tag => tag.name)
+        }
     }
 }
