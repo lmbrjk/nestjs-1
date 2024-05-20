a156d194e43597d72cb6bf052d64b52ec33b5419
Evgeny <evgeny@MacBook-Air-Evgeny.local>
2024-05-20 16:29:31 +0700

    add typeORM, mysql and entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,22 @@ import { AppController } from '@app/app.controller';
 import { AppService } from '@app/app.service';
 import { DatabaseModule } from '@app/database/database.module';
 import { TagModule } from '@app/tag/tag.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TagEntity } from '@app/tag/entities/tag.entity';
 
 @Module({
-  imports: [DatabaseModule, TagModule],
+  imports: [
+    TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '',
+    database: 'nest',
+    entities: [TagEntity],
+    // entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true,
+  }), DatabaseModule, TagModule],
   controllers: [AppController],
   providers: [AppService],
 })
