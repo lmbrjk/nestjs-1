ee752fce8f48025d0c63a2de95142484ddac500d
e.vyunov <e.vyunov@els24.com>
2024-07-02 17:59:09 +0700

    create article module, service, controller and etc

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import ormconfig from './ormconfig';
 import { UserModule } from './user/user.module';
 import { AuthMiddleware } from './user/middlewares/auth.middleware';
+import { ArticleModule } from './article/article.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(ormconfig), DatabaseModule, TagModule, UserModule],
+    TypeOrmModule.forRoot(ormconfig), DatabaseModule, TagModule, UserModule, ArticleModule],
   controllers: [AppController],
   providers: [AppService],
 })
